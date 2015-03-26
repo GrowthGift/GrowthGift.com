f107af4d1b1c2d0fef268de47149671505270a8f
Luke Madera <luke.madera@gmail.com>
2015-03-25 21:15:57 -0700

    feat(accounts-password): allow enrollAccount multiple email templates

--- packages/accounts-password/accounts-password.js
+++ packages/accounts-password/accounts-password.js
@@ -1,8 +1,8 @@
 AccountsPassword ={};
 
 Meteor.methods({
-  lmAccountsPasswordSignupUser: function(userData, params) {
-    return AccountsPassword.signupUser(userData, params);
+  lmAccountsPasswordSignupUser: function(userData, type, params) {
+    return AccountsPassword.signupUser(userData, type, params);
   }
 });
 
@@ -13,17 +13,8 @@ if(Meteor.isServer) {
     Accounts.emailTemplates.siteName =Config.appInfo().name;
 
     //reset password
-    var formResetPasswordUrl =function(url) {
-      var index1 =url.lastIndexOf('/');
-      var token =url.slice((index1+1), url.length);
-      index1 =url.indexOf('#');
-      var host =url.slice(0, index1);
-      newUrl =host+'reset-password?token='+token;
-      return newUrl;
-    };
-
     Accounts.emailTemplates.resetPassword.text =function(user, url) {
-      url =formResetPasswordUrl(url);
+      url =AccountsPassword.formResetPasswordUrl(url);
       var content ="Hi "+user.profile.name+",\n\n"+
         "Click this link to reset your password:\n"+
         url+"\n";
@@ -31,37 +22,61 @@ if(Meteor.isServer) {
     };
 
     Accounts.emailTemplates.resetPassword.html =function(user, url) {
-      url =formResetPasswordUrl(url);
+      url =AccountsPassword.formResetPasswordUrl(url);
       var content ="Hi "+user.profile.name+",<br /><br />"+
         "<a href='"+url+"'>Click here to reset your password.</a><br />";
       return content;
     };
 
-    //enroll account (sign up new user)
-    var formEnrollUrl =function(url) {
-      var index1 =url.lastIndexOf('/');
-      var token =url.slice((index1+1), url.length);
-      index1 =url.indexOf('#');
-      var host =url.slice(0, index1);
-      newUrl =host+'enroll-account?token='+token;
-      return newUrl;
-    };
-    
-    Accounts.emailTemplates.enrollAccount.text =function(user, url) {
-      url =formEnrollUrl(url);
-      var content ="Hi "+user.profile.name+",\n\n"+
-        "To set a password and start using the service, simply click the link below.\n"+
-        url+"\n";
-      return content; 
-    };
-
-    Accounts.emailTemplates.enrollAccount.html =function(user, url) {
-      url =formEnrollUrl(url);
-      var content ="Hi "+user.profile.name+",<br /><br />"+
-        "<a href='"+url+"'>Click here to set a password and start using the service.</a><br />";
-      return content;
-    };
+    //[enroll account (sign up new user) is set dynamically right before sending since may have multiple templates]
   }
+
+  AccountsPassword.formResetPasswordUrl =function(url) {
+    var index1 =url.lastIndexOf('/');
+    var token =url.slice((index1+1), url.length);
+    index1 =url.indexOf('#');
+    var host =url.slice(0, index1);
+    newUrl =host+'reset-password?token='+token;
+    return newUrl;
+  };
+
+  AccountsPassword.formEnrollUrl =function(url) {
+    var index1 =url.lastIndexOf('/');
+    var token =url.slice((index1+1), url.length);
+    index1 =url.indexOf('#');
+    var host =url.slice(0, index1);
+    newUrl =host+'enroll-account?token='+token;
+    return newUrl;
+  };
+
+  /**
+  @param {String} [type ='default'] One of 'default'
+  @param {Object} [params]
+  */
+  AccountsPassword.setEnrollAccountEmail =function(type, params) {
+    if(type ===undefined) {
+      type ='default';
+    }
+    if(type ==='todoseed') {
+      //@todo - set custom templates as needed based on type
+    }
+    else {
+      Accounts.emailTemplates.enrollAccount.text =function(user, url) {
+        url =AccountsPassword.formEnrollUrl(url);
+        var content ="Hi "+user.profile.name+",\n\n"+
+          "To set a password and start using the service, simply click the link below.\n"+
+          url+"\n";
+        return content; 
+      };
+
+      Accounts.emailTemplates.enrollAccount.html =function(user, url) {
+        url =AccountsPassword.formEnrollUrl(url);
+        var content ="Hi "+user.profile.name+",<br /><br />"+
+          "<a href='"+url+"'>Click here to set a password and start using the service.</a><br />";
+        return content;
+      };
+    }
+  };
 
   //init
   Meteor.startup(function() {
@@ -75,9 +90,15 @@ if(Meteor.isServer) {
   @param {String} [password] If none set, an enrollment email will be sent out to the email for the user to set their password
   @param {Object} profile At least 'name' should be set
     @param {String} name
+@param {String} [type ='default'] One of 'default'
+@param {Object} [params]
+  @param {Object} [emailParams] Params that are passed through to setEnrollAccountEmail [see there for documentation]
 */
-AccountsPassword.signupUser =function(userData, params) {
+AccountsPassword.signupUser =function(userData, type, params) {
   if(Meteor.isServer) {
+    if(params ===undefined) {
+      params ={};
+    }
     // if(userData.password ===undefined) {
     //   userData.password =lmString.random(20, {type:'readable'});
     // }
@@ -85,6 +106,7 @@ AccountsPassword.signupUser =function(userData, params) {
     var userId =Accounts.createUser(userData);
     //if no password, send email to have user set password
     if(userData.password ===undefined) {
+      this.setEnrollAccountEmail(type, params.emailParams);
       Accounts.sendEnrollmentEmail(userId);
     }
   }
