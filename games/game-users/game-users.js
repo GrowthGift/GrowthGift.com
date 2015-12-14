805417ef8eb9893fb3aaed4fff3723ac9cd0248e
Luke Madera <luke.madera@gmail.com>
2015-12-14 14:07:12 -0800

    feat: make game-users columns sortable

--- games/game-users/game-users.js
+++ games/game-users/game-users.js
@@ -1,6 +1,53 @@
 if(Meteor.isClient) {
+  function formatUsers(users, sortKeys, sortOrders) {
+    users.forEach(function(user) {
+      if(user) {
+        user.xDisplay ={
+          user1: {
+            href: ( user.user1.username ) ? ggUrls.user(user.user1.username) : ''
+          },
+          user2: {
+            href: ( user.user2.username ) ? ggUrls.user(user.user2.username) : ''
+          },
+          displayHtml: null,
+          classes: {
+            row: ''
+          }
+        };
+        if(user.user1._id && user.user2._id) {
+          // user.xDisplay.displayHtml ='<a href=' + user.xDisplay.user1.href +
+          //  '>' + ggUser.getName(user.user1) + '</a> & <a href=' +
+          //  user.xDisplay.user2.href + '>' + ggUser.getName(user.user2) + '</a>';
+          user.xDisplay.displayHtml = ggUser.getName(user.user1) + ' & ' + ggUser.getName(user.user2);
+        }
+        else if(user.user1._id) {
+          // user.xDisplay.displayHtml ='*<a href=' + user.xDisplay.user1.href
+          //  + '>' + ggUser.getName(user.user1) + '</a>';
+          user.xDisplay.displayHtml ='*' + ggUser.getName(user.user1);
+          if(user.user1._id ===Meteor.userId()) {
+            user.xDisplay.classes.row ='self';
+          }
+        }
+        else if(user.user2._id) {
+          // user.xDisplay.displayHtml ='*<a href=' + user.xDisplay.user2.href
+          //  + '>' + ggUser.getName(user.user2) + '</a>';
+          user.xDisplay.displayHtml ='*' + ggUser.getName(user.user2);
+          if(user.user2._id ===Meteor.userId()) {
+            user.xDisplay.classes.row ='self';
+          }
+        }
+      }
+    });
+    if(sortKeys && sortOrders) {
+      users =_.sortByOrder(users, sortKeys, sortOrders);
+    }
+    return users;
+  }
+
   Template.gameUsers.created =function() {
     Meteor.subscribe('game', Template.instance().data.gameSlug);
+    this.users =new ReactiveVar([]);
+    this.inited =false;
   };
 
   Template.gameUsers.helpers({
@@ -21,46 +68,17 @@ if(Meteor.isClient) {
       }
 
       var gameUsers =ggGame.getGameUsersInfo(userGames);
-      var users =ggGame.getGameUsersStats(userGames, game, gameUsers, gameRule, null);
-      users.forEach(function(user) {
-        if(user) {
-          user.xDisplay ={
-            user1: {
-              href: ( user.user1.username ) ? ggUrls.user(user.user1.username) : ''
-            },
-            user2: {
-              href: ( user.user2.username ) ? ggUrls.user(user.user2.username) : ''
-            },
-            displayHtml: null,
-            classes: {
-              row: ''
-            }
-          };
-          if(user.user1._id && user.user2._id) {
-            // user.xDisplay.displayHtml ='<a href=' + user.xDisplay.user1.href +
-            //  '>' + ggUser.getName(user.user1) + '</a> & <a href=' +
-            //  user.xDisplay.user2.href + '>' + ggUser.getName(user.user2) + '</a>';
-            user.xDisplay.displayHtml = ggUser.getName(user.user1) + ' & ' + ggUser.getName(user.user2);
-          }
-          else if(user.user1._id) {
-            // user.xDisplay.displayHtml ='*<a href=' + user.xDisplay.user1.href
-            //  + '>' + ggUser.getName(user.user1) + '</a>';
-            user.xDisplay.displayHtml ='*' + ggUser.getName(user.user1);
-            if(user.user1._id ===Meteor.userId()) {
-              user.xDisplay.classes.row ='self';
-            }
-          }
-          else if(user.user2._id) {
-            // user.xDisplay.displayHtml ='*<a href=' + user.xDisplay.user2.href
-            //  + '>' + ggUser.getName(user.user2) + '</a>';
-            user.xDisplay.displayHtml ='*' + ggUser.getName(user.user2);
-            if(user.user2._id ===Meteor.userId()) {
-              user.xDisplay.classes.row ='self';
-            }
-          }
-        }
-      });
-      users =_.sortByOrder(users, ['buddiedPledgePercent'], ['desc']);
+      var users;
+      var reactiveUsers =Template.instance().users.get();
+      if(!Template.instance().inited) {
+        Template.instance().inited =true;
+        users =ggGame.getGameUsersStats(userGames, game, gameUsers, gameRule, null);
+        users =formatUsers(users, ['buddiedPledgePercent'], ['desc']);
+        Template.instance().users.set(users);
+      }
+      else {
+        users =Template.instance().users.get();
+      }
       return {
         game: game,
         gameRule: gameRule,
@@ -70,6 +88,24 @@ if(Meteor.isClient) {
         users: users,
         challengeTotals: ggGame.getChallengeTotals(game, userGames, gameRule, null)
       };
+    }
+  });
+
+  Template.gameUsers.events({
+    'click .game-users-header-buddy': function(evt, template) {
+      var users =template.users.get();
+      users =_.sortByOrder(users, ['xDisplay.displayHtml'], ['asc']);
+      template.users.set(users);
+    },
+    'click .game-users-header-pledge': function(evt, template) {
+      var users =template.users.get();
+      users =_.sortByOrder(users, ['buddiedPledgePercent'], ['desc']);
+      template.users.set(users);
+    },
+    'click .game-users-header-impact': function(evt, template) {
+      var users =template.users.get();
+      users =_.sortByOrder(users, ['buddiedReachTeamsNumActions'], ['desc']);
+      template.users.set(users);
     }
   });
 }
