b224c13cdfb861336af4d1656c228003513e3459
Luke Madera <luke.madera@gmail.com>
2015-12-20 17:58:21 -0800

    minor: show awards even before game starts (for reach), switch home route to new planks game.

--- nav/menu/menu.js
+++ nav/menu/menu.js
@@ -27,7 +27,7 @@ if (Meteor.isClient){
     // routeTo: "home"
     // TEMPOARARY hardcoded current (promoted) game
     // Home should work but was going to login page..
-    routeTo: '/g/pushups'
+    routeTo: '/g/planks'
   };
   var menuItemLanding ={
     icon: "fa fa-home",
@@ -35,7 +35,7 @@ if (Meteor.isClient){
     // routeTo: "home"
     // TEMPOARARY hardcoded current (promoted) game
     // Home should work but was going to login page..
-    routeTo: '/g/pushups'
+    routeTo: '/g/planks'
   };
 
   var menuItemAbout ={
